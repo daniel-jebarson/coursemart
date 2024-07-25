ac032a33028c9bb4ca43eb3acc6af873c362255b
daniel-jebarson <danieljebarson21@gmail.com>
2024-07-25 22:44:42 +0530

    ✨ Route to update info of institute

--- server/src/routes/institute.js
+++ server/src/routes/institute.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { registerFaculty } = require("../controllers/faculty");
-const { addInstituteData } = require("../controllers/institute");
+const {
+  addInstituteData,
+  updateInstituteData,
+} = require("../controllers/institute");
 const { authorizer } = require("../middleware/auth/auth");
 
 const router = express.Router();
 
 router.route("/faculty").post(authorizer, registerFaculty);
-router.route("/info").post(authorizer, addInstituteData);
+router
+  .route("/info")
+  .post(authorizer, addInstituteData)
+  .put(authorizer, updateInstituteData);
 
 module.exports = router;
