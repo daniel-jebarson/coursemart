30389e5b8cf6f539bd2c9ebf719f218a7f827cc8
daniel-jebarson <danieljebarson21@gmail.com>
2024-08-24 17:20:35 +0530

    ✨ Added filter for courses

--- server/src/routes/course.js
+++ server/src/routes/course.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   registerCourse,
   getCourseByInstituteId,
+  getCourseByFilter,
 } = require("../controllers/course.js");
 const { authorizer } = require("../middleware/auth/auth");
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.route("/register").post(authorizer, registerCourse);
 router.route("/institute/:instituteId").get(authorizer, getCourseByInstituteId);
+router.route("/search").get(authorizer, getCourseByFilter);
 
 module.exports = router;
