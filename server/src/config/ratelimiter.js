cfc365fd0fc826af75fc58e650152b96551ebcf9
daniel-jebarson <danieljebarson21@gmail.com>
2024-08-31 16:50:08 +0530

    🎨 Added rate limiter

---
+++ server/src/config/ratelimiter.js
@@ -0,0 +1,37 @@
+const rateLimit = require("express-rate-limit");
+const colors = require("colors");
+
+const getRemainingTimeInSeconds = (resetTime) => {
+  const now = Date.now();
+  const resetTimestamp = new Date(resetTime).getTime();
+  const remainingMillis = resetTimestamp - now;
+
+  // Calculate remaining time in seconds
+  return Math.max(Math.floor(remainingMillis / 1000), 0);
+};
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 150, // Limit each IP to 150 requests per `windowMs`
+  message: "Too many requests from this IP, please try again later.",
+  headers: true, // Send rate limit info in the `X-RateLimit-*` headers
+  handler: (req, res) => {
+    const remainingTimeInSeconds = getRemainingTimeInSeconds(
+      req.rateLimit.resetTime
+    );
+    res.status(429).json({
+      name: "RateLimitExceededError",
+      msg: "You have exceeded the number of allowed requests. Please try again later.",
+      rateLimit: {
+        max: req.rateLimit.max, // Max requests
+        remaining: req.rateLimit.remaining, // Remaining requests
+        resetInSeconds: remainingTimeInSeconds, // Remaining time in seconds
+      },
+    });
+  },
+  onLimitReached: (req, res, options) => {
+    console.log(`Rate limit exceeded for IP ${req.ip}`.cyan.bold);
+  },
+});
+
+module.exports = limiter;
